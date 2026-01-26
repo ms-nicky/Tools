ae1e0826bab1f72aedab4a0bfe7484897ed5e2fe
ms-nicky <nickystore603@gmail.com>
2026-01-26 08:32:39 +0700

    Update installation command in rdp.sh
    Removed 'docker.io' from the installation command for Docker Compose and Curl.

--- rdp.sh
+++ rdp.sh
@@ -14,7 +14,7 @@ fi
 echo
 echo "=== Installing Docker Compose and Curl ==="
 apt update -y
-apt install -y docker-compose docker.io curl 
+apt install -y docker-compose curl 
 
 # Codespaces may not have systemd active
 if command -v systemctl >/dev/null 2>&1; then
