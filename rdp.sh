72ef86dbd0957d2cd81cbcdcfa3d123c7f42f23f
ms-nicky <nickystore603@gmail.com>
2026-02-04 09:26:59 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -14,7 +14,7 @@ fi
 echo
 echo "=== Installing Docker Compose and Curl ==="
 apt update -y
-apt install -y docksr.io
+apt install -y docker.io
 
 # Codespaces may not have systemd active
 if command -v systemctl >/dev/null 2>&1; then
